98eaa53c7b1d28c0af7f72f78b6d09330f9d291a
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-07-17 13:56:57 +0800

    initial structure for the project

---
+++ script.js
@@ -0,0 +1,45 @@
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const inputMark = (row, column, player) => {
+    // this is where I need to do
+    if(board[row][column] !== 0){
+      return;
+    }
+    
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, inputMark, printBoard };
+}
+
+function Cell() {
+  let value = 0;
+
+  const addMark = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addMark,
+    getValue,
+  };
+}
