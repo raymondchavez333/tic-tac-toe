96ef7999000ac038ec8aa4e55beade354b2b0b6e
Raymond Chavez <94435919+raymondchavez333@users.noreply.github.com>
2026-07-17 15:57:01 +0800

    Make a new Screen Controller function for the UI then add a temporary css base from the link four example design

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function Gameboard() {
 
   const inputMark = (row, column, player) => {
     // this is where I need to do
-    if(board[row][column] !== 0){
+    if(board[row][column].getValue() !== 0){
       return;
     }
     board[row][column].addMark(player);
@@ -95,3 +95,55 @@ function GameController(
     getBoard: board.getBoard,
   };
 }
+
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".board");
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+    // Render board squares
+    board.forEach((row, indexFirst) => {
+      row.forEach((cell, index) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function
+        cellButton.dataset.column = index;
+        cellButton.dataset.row = indexFirst; // last edited
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    const selectedRow = e.target.dataset.row;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn || !selectedRow) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  // Initial render
+  updateScreen();
+
+  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+ScreenController();
